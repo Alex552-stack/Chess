94a5509a439a39b47aa553293092a103ad32b6af
AVRAM MATEI-ALEXANDRU <avramalexandru2030@gmail.com>
2025-03-22 14:54:45 +0200

    board orientation based. next step back end validation of player color. this will probably come with a total redesign of the back end. The Move method need to also somehow return a boolean to the caller to signify is the move was valid or not because this causes desync between the clients

--- ChessWebUi/ChessWebUi/src/pages/GamePage.tsx
+++ ChessWebUi/ChessWebUi/src/pages/GamePage.tsx
@@ -3,6 +3,7 @@ import { useSignalR } from "../hooks/useSignalR";
 import { makeMove } from "../services/chessService";
 import {useEffect, useState} from "react";
 import {Chess} from "chess.js";
+import {Colors} from "../models/Colors.ts";
 
 export default function GamePage() {
     //const [game, setGame] = useState(new Chess());
@@ -14,6 +15,7 @@ export default function GamePage() {
     }, [game])
 
     const handleMove = async (source: string, target: string, piece: any) => {
+
         // Optimistically update board
         const gameCopy = new Chess(localGame?.fen());
         const move = gameCopy.move({
@@ -35,7 +37,7 @@ export default function GamePage() {
     return (
         <div>
             <button onClick={() => createGame(connection)}>Create Game</button>
-            <ChessboardComponent position={localGame?.fen()} handleMove={handleMove} />
+            <ChessboardComponent position={localGame?.fen()} handleMove={handleMove} color={color}/>
         </div>
     );
 }
